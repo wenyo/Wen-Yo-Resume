aac197cfcf944ff18e6d579af4f7c8c5e95f1f31
wenyo <wenda897436@gmail.com>
2019-03-06 20:11:05 +0800

    revice pic url

--- all.js
+++ all.js
@@ -3,35 +3,35 @@
 // Data
 const skillData = {
     'HTML':{
-        'skillItemimg' : '../../images/html-logo.png',
+        'skillItemimg' : './images/html-logo.png',
         'proficiency' : 'master',
         'percentage' : '60%',
         'profiWord' : 'HTML / HTML5 ( 擅長 )',
         'explain' : '能有效率的使用語意化標籤。'
     },
     'CSS':{
-        'skillItemimg' : '../../images/css-logo.png',
+        'skillItemimg' : './images/css-logo.png',
         'proficiency' : 'familiar',
         'percentage' : '50%',
         'profiWord' : 'CSS 3 / SCSS ( 熟悉 )',
         'explain' : '能撰寫語意化標籤、排版、flexbox。' 
     },
     'RWD':{
-        'skillItemimg' : '../../images/rwd-logo.png',
+        'skillItemimg' : './images/rwd-logo.png',
         'proficiency' : 'familiar',
         'percentage' : '50%',
         'profiWord' : 'Responsive Web Design ( 熟悉 )',
         'explain' : '能手刻出自適應網頁設計。' 
     },
     'JavaScript':{
-        'skillItemimg' : '../../images/javascript-logo.png',
+        'skillItemimg' : './images/javascript-logo.png',
         'proficiency' : 'familiar',
         'percentage' : '50%',
         'profiWord' : 'JavaScript ( 熟悉 )',
         'explain' : '熟悉DOM模組、 this 、閉包、 AJAX等概念。' 
     },
     'Vue.js':{
-        'skillItemimg' : '../../images/vue-logo.png',
+        'skillItemimg' : './images/vue-logo.png',
         'proficiency' : 'common',
         'percentage' : '20%',
         'profiWord' : 'Vue.js ( 略懂 )',
