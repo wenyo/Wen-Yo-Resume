45aa6c375c78cc97ce30a403a63ff58e80ffb988
wenyo <wenda897436@gmail.com>
2020-05-22 00:07:03 +0800

    #0522 2020 調整樣式 更換banner
    換色
    修正轉換螢幕尺寸時banner高度

--- js/all.js
+++ js/all.js
@@ -1,73 +1,10 @@
-// --**Data**--//
-const skillData = {
-    'HTML':{
-        'skillItemimg' : './images/html-logo.png',
-        'proficiency' : 'master',
-        'percentage': '60%',
-        'percentagePhone' : '80%',
-        'profiWord' : 'HTML / HTML5<br>( 擅長 )',
-        'explain' : '能有效率的使用語意化標籤。'
-    },
-    'CSS':{
-        'skillItemimg' : './images/css-logo.png',
-        'proficiency' : 'familiar',
-        'percentage' : '50%',
-        'percentagePhone' :'70%',
-        'profiWord' : 'CSS 3 / SCSS<br>( 熟悉 )',
-        'explain' : '能撰寫語意化標籤、排版、flexbox。' 
-    },
-    'RWD':{
-        'skillItemimg' : './images/rwd-logo.png',
-        'proficiency' : 'familiar',
-        'percentage' : '50%',
-        'percentagePhone' :'70%',
-        'profiWord' : 'RWD<br>( 熟悉 )',
-        'explain' : '能手刻出自適應網頁設計。' 
-    },
-    'JavaScript':{
-        'skillItemimg' : './images/javascript-logo.png',
-        'proficiency' : 'familiar',
-        'percentage' : '50%',
-        'percentagePhone' :'70%',
-        'profiWord' : 'JavaScript<br>( 熟悉 )',
-        'explain' : '熟悉DOM模組、 this 、閉包、 AJAX等概念。' 
-    },
-    'Vue.js':{
-        'skillItemimg' : './images/vue-logo.png',
-        'proficiency' : 'common',
-        'percentage' : '20%',
-        'percentagePhone' :'50%',
-        'profiWord' : 'Vue.js<br>( 略懂 )',
-        'explain' : '初步學習  v-model、v-on等。' 
-    },
-}
-const workData = {
-    'kaohsiungTraveling':{
-        'name' : '高雄旅遊資訊',
-        'github' : 'https://github.com/wenyo/kaohsiungTraveling',
-        'web' : 'https://wenyo.github.io/kaohsiungTraveling/',
-        'img' : './images/kaohsiungTraveling.png',
-        'imgHover' : './images/kaohsiungTraveling-hover.png',
-    },
-    'BMI':{
-        'name' : '身體質量指數計算',
-        'github' : 'https://github.com/wenyo/BMI-website',
-        'web' : 'https://wenyo.github.io/BMI-website/',
-        'img' : './images/BMI.png',
-        'imgHover' : './images/BMI-hover.png',
-    },
-    'RWD-HomeWork':{
-        'name' : '六角西餐廳',
-        'github' : 'https://github.com/wenyo/RWD-HomeWork',
-        'web' : 'https://wenyo.github.io/RWD-HomeWork/',
-        'img' : './images/RWD-HomeWork.png',
-        'imgHover' : './images/RWD-HomeWork-hover.png',
-    }
-};
-
 //--** Header CSS Start **--//
-const banner = document.querySelector('.header .content');
-banner.style.height =  (document.documentElement.clientHeight) + 'px';
+function changeBannerHeight(){
+    const banner = document.querySelector('.header .content');
+    banner.style.height =  (document.documentElement.clientHeight) + 'px';
+}
+changeBannerHeight();
+window.onresize = changeBannerHeight;
 
 // HamburderMenu
 const hambergerIcon = document.querySelector('.hambergerIcon');
@@ -98,8 +35,10 @@ for(const key in skillData){
         <div class="skillItem  ${key}">
             <img src="${img}">
         </div>
-        <div class="proficiency">${profiWord}</div>
-        <div class="explain">${explain}</div>
+        <div class="proficiency">
+            <span>${profiWord}</span>
+            <div class="explain">${explain}</div>
+        </div>
     </li>`;
 
     const proficiencyClazz = document.querySelectorAll('.proficiency');
@@ -108,7 +47,7 @@ for(const key in skillData){
     if(document.documentElement.clientWidth > 700){
         for(let i = 0 ; i < proficiencyClazz.length ; i++ ){
             proficiencyClazz[i].style.width += skillData[key].percentage;
-            explainClazz[i].style.left += skillData[key].percentage;
+            explainClazz[i].style.left += skillData[key].percentage + 0.2;
         }
     }else{
         for(let i = 0 ; i < proficiencyClazz.length ; i++ ){
---
+++ js/data.js
@@ -0,0 +1,67 @@
+// --**Data**--//
+const skillData = {
+    'HTML':{
+        'skillItemimg' : './images/html-logo.png',
+        'proficiency' : 'master',
+        'percentage': '60%',
+        'percentagePhone' : '80%',
+        'profiWord' : 'HTML / HTML5<br>( 擅長 )',
+        'explain' : '能有效率的使用語意化標籤。'
+    },
+    'CSS':{
+        'skillItemimg' : './images/css-logo.png',
+        'proficiency' : 'familiar',
+        'percentage' : '50%',
+        'percentagePhone' :'70%',
+        'profiWord' : 'CSS 3 / SCSS<br>( 熟悉 )',
+        'explain' : '能撰寫語意化標籤、排版、flexbox。' 
+    },
+    'RWD':{
+        'skillItemimg' : './images/rwd-logo.png',
+        'proficiency' : 'familiar',
+        'percentage' : '50%',
+        'percentagePhone' :'70%',
+        'profiWord' : 'RWD<br>( 熟悉 )',
+        'explain' : '能手刻出自適應網頁設計。' 
+    },
+    'JavaScript':{
+        'skillItemimg' : './images/javascript-logo.png',
+        'proficiency' : 'familiar',
+        'percentage' : '50%',
+        'percentagePhone' :'70%',
+        'profiWord' : 'JavaScript<br>( 熟悉 )',
+        'explain' : '熟悉DOM模組、 this 、閉包、 AJAX等概念。' 
+    },
+    'Vue.js':{
+        'skillItemimg' : './images/vue-logo.png',
+        'proficiency' : 'common',
+        'percentage' : '20%',
+        'percentagePhone' :'50%',
+        'profiWord' : 'Vue.js<br>( 略懂 )',
+        'explain' : '初步學習  v-model、v-on等。' 
+    },
+}
+
+const workData = {
+    'kaohsiungTraveling':{
+        'name' : '高雄旅遊資訊',
+        'github' : 'https://github.com/wenyo/kaohsiungTraveling',
+        'web' : 'https://wenyo.github.io/kaohsiungTraveling/',
+        'img' : './images/kaohsiungTraveling.png',
+        'imgHover' : './images/kaohsiungTraveling-hover.png',
+    },
+    'BMI':{
+        'name' : '身體質量指數計算',
+        'github' : 'https://github.com/wenyo/BMI-website',
+        'web' : 'https://wenyo.github.io/BMI-website/',
+        'img' : './images/BMI.png',
+        'imgHover' : './images/BMI-hover.png',
+    },
+    'RWD-HomeWork':{
+        'name' : '六角西餐廳',
+        'github' : 'https://github.com/wenyo/RWD-HomeWork',
+        'web' : 'https://wenyo.github.io/RWD-HomeWork/',
+        'img' : './images/RWD-HomeWork.png',
+        'imgHover' : './images/RWD-HomeWork-hover.png',
+    }
+};
